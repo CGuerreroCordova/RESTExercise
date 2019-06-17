30838b6812eb65509649fe7591c2ffba187c2055
cguerrerocordova@gmail.com <cguerrerocordova@gmail.com>
2019-06-17 18:58:07 +0200

    Empty Message

--- mangiato/__main__.py
+++
@@ -1,9 +0,0 @@
-"""
-Module of execution of Mangiato API
-"""
-
-from mangiato.mangiato_api import APP, initialize_app, run_app, add_users
-
-if __name__ == '__main__': # pragma: no cover
-    initialize_app(APP, "app_config.py")
-    run_app()
--- mangiato/api/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Instance and provide blueprint registered for the API
-"""
-from flask import Blueprint
-
-
-BLUEPRINT = Blueprint('api', __name__)
